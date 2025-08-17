6db3bd36aefaa9164e1ac1ea288885bced8861b8
Konstantin Umanets <konstantin_umanets@epam.com>
2025-08-17 22:06:32 +0400

    Fix code

--- src/main/java/com/epam/oneendpoint/SimpleController.java
+++ src/main/java/com/epam/oneendpoint/SimpleController.java
@@ -3,11 +3,8 @@ package com.epam.oneendpoint;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import software.amazon.awssdk.core.document.Document;
 import software.amazon.awssdk.imds.Ec2MetadataClient;
 import software.amazon.awssdk.imds.Ec2MetadataResponse;
-
-import java.util.Map;
 
 @RestController
 public class SimpleController {
@@ -15,10 +12,14 @@ public class SimpleController {
     @GetMapping("/metadata")
     public ResponseEntity<String> getMetadata() {
         try (Ec2MetadataClient client = Ec2MetadataClient.create()) {
-            Ec2MetadataResponse response = client.get("/latest/meta-data/");
-            Document document = response.asDocument();
-            Map<String, Document> mapResults = document.asMap();
-            return ResponseEntity.ok(response.asString());
+            Ec2MetadataResponse regionResponse = client.get("/latest/meta-data/placement/region");
+            String region = regionResponse.asString();
+
+            Ec2MetadataResponse response = client.get("/latest/meta-data/placement/availability-zone");
+            String availabilityZone = response.asString();
+//            System.out.println("Region: " + region);
+//            System.out.println("AZ: " + availabilityZone);
+            return ResponseEntity.ok("Region: [" + region + "], AZ: [" + availabilityZone + "]");
         }
     }
 
