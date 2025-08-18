55db57f433602ab9be3c769366a63ef661f2013d
Konstantin Umanets <konstantin_umanets@epam.com>
2025-08-18 12:11:24 +0400

    Fix code

--- src/main/java/com/epam/oneendpoint/SimpleController.java
+++ src/main/java/com/epam/oneendpoint/SimpleController.java
@@ -9,22 +9,17 @@ import software.amazon.awssdk.imds.Ec2MetadataResponse;
 @RestController
 public class SimpleController {
 
+    private static final String BASE_METADATA_URL = "/latest/meta-data/placement";
+
     @GetMapping("/metadata")
     public ResponseEntity<String> getMetadata() {
         try (Ec2MetadataClient client = Ec2MetadataClient.create()) {
-            Ec2MetadataResponse regionResponse = client.get("/latest/meta-data/placement/region");
+            Ec2MetadataResponse regionResponse = client.get(BASE_METADATA_URL + "/region");
             String region = regionResponse.asString();
 
-            Ec2MetadataResponse response = client.get("/latest/meta-data/placement/availability-zone");
+            Ec2MetadataResponse response = client.get(BASE_METADATA_URL + "/availability-zone");
             String availabilityZone = response.asString();
-//            System.out.println("Region: " + region);
-//            System.out.println("AZ: " + availabilityZone);
             return ResponseEntity.ok("Region: [" + region + "], AZ: [" + availabilityZone + "]");
         }
-    }
-
-    @GetMapping("/hello")
-    public ResponseEntity<String> getHello() {
-        return ResponseEntity.ok("hello!");
     }
 }
